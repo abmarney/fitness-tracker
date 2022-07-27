fca8baa4dced5386db9f9d02409e1b09d83808a3
abmarney <alexander.marney@gmail.com>
2022-07-27 11:24:26 -0700

    Exercise updatable and deletable

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+exercise_count = 0
 
 class Exercise(db.Model):
     name = db.Column(db.String(200), primary_key=True)
     reps = db.Column(db.Integer)
     sets = db.Column(db.Integer)
+    #id = db.Column(db.Integer)
     def __repr__(self):
         return self.name
 
 class Workout(db.Model):
     name = db.Column(db.String(200), primary_key=True)
+    #routine = [Exercise()]
+    #count = len(routine)
     def __repr__(self):
         return self.name
 
@@ -35,7 +39,33 @@ def index():
         exercises=Exercise.query.order_by(Exercise.name).all()
         return render_template('index.html', exercises=exercises)
 
+@app.route('/delete/Exercise/<string:name>')
+def exercise_delete(name):
+    exercise_to_delete = Exercise.query.get_or_404(name)
+
+    try:
+        db.session.delete(exercise_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deletingg this exercise.'
+
+@app.route('/update/Exercise/<string:name>', methods=['POST','GET'])
+def exercise_update(name):
+    exercise_to_update = Exercise.query.get_or_404(name)
+
+    if request.method == 'POST':
+        exercise_to_update.name = request.form['name']
+        exercise_to_update.reps = request.form['reps']
+        exercise_to_update.sets = request.form['sets']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating this exercise.'
+    else:
+        return render_template('update.html', exercise=exercise_to_update)
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
