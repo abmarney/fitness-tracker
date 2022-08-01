80fb11f250c8aad5c4d6b72e0e85d7174b06bec4
Alexander Marney <alexander.marney@gmail.com>
2022-08-01 11:50:43 -0700

    Reworked variable names, workout object functional

--- app.py
+++ app.py
@@ -1,80 +1,81 @@
+from distutils.command.build_ext import extension_name_re
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-exercise_count = 0
 
-class Exercise(db.Model):
-    name = db.Column(db.String(200), primary_key=True)
-    reps = db.Column(db.Integer)
-    sets = db.Column(db.Integer)
-    id = db.Column(db.Integer)
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    routine = db.relationship('Exercise', backref='workout')
+
     def __repr__(self):
         return self.name
 
-class Workout(db.Model):
-    name = db.Column(db.String(200), primary_key=True)
-    routine = [Exercise()]
-    count = len(routine)
+class Exercise(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(200), nullable=False)
+    reps=db.Column(db.Integer, nullable=False)
+    sets=db.Column(db.Integer, nullable=False)
+    workout_id=db.Column(db.Integer, db.ForeignKey('workout.id'))
+
     def __repr__(self):
         return self.name
 
 @app.route('/')
 def index():
-    routines=Workout.query.order_by(Workout.name).all()
-    return render_template('index.html', routines=routines)
+    workouts=Workout.query.order_by(Workout.id).all()
+    return render_template('index.html', workouts=workouts)
 
-@app.route('/CreateRoutine/', methods=['POST','GET'])
+@app.route('/CreateWorkout/', methods=['POST','GET'])
 def create_routine():
-    return render_template('routines.html')
-    
-@app.route('/Exercises/', methods=['POST','GET'])
-def routine():
+    workout_to_create=Workout.query.order_by(Workout.id).all()
+
     if request.method == 'POST':
-        ex_name = request.form['name']
-        ex_reps = request.form['reps']
-        ex_sets = request.form['sets']
-        new_exercise = Exercise(name=ex_name,reps=ex_reps,sets=ex_sets)
-        
+        workout_name = request.form['name']
+        new_workout = Workout(name=workout_name)
+
         try:
-            db.session.add(new_exercise)
+            db.session.add(new_workout)
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an error creating your exercise'
+            return 'There was an issue creating your workout.'
     else:
-        exercises=Exercise.query.order_by(Exercise.name).all()
-        return render_template('index.html', exercises=exercises)
+        return render_template('workouts.html', workout=workout_to_create)
 
-@app.route('/delete/Exercise/<string:name>')
-def exercise_delete(name):
-    exercise_to_delete = Exercise.query.get_or_404(name)
+@app.route('/delete/<int:id>')
+def routine_delete(id):
+    workout_to_delete = Workout.query.get_or_404(id)
 
     try:
-        db.session.delete(exercise_to_delete)
+        db.session.delete(workout_to_delete)
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deletingg this exercise.'
+        return 'There was a problem deleting this routine.'
 
-@app.route('/update/Exercise/<string:name>', methods=['POST','GET'])
-def exercise_update(name):
-    exercise_to_update = Exercise.query.get_or_404(name)
+@app.route('/workout/<int:id>')
+def show_routine(id):
+    workout_to_show = Workout.query.get_or_404(id)
+    return render_template('workouts.html', workout=workout_to_show)
 
-    if request.method == 'POST':
-        exercise_to_update.name = request.form['name']
-        exercise_to_update.reps = request.form['reps']
-        exercise_to_update.sets = request.form['sets']
+@app.route('/update/<int:id>', methods=['POST'])
+def update_routine(id):
+    workout_to_update = Workout.query.get_or_404(id)
+    ex_name = request.form['name']
+    ex_reps = request.form['reps']
+    ex_sets = request.form['sets']
+    new_exercise = Exercise(name=ex_name,reps=ex_reps,sets=ex_sets,workout=workout_to_update)
 
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating this exercise.'
-    else:
-        return render_template('update.html', exercise=exercise_to_update)
+    try:
+        db.session.add(new_exercise)
+        db.session.commit()
+        return redirect(f'/workout/{workout_to_update.id}')
+    except:
+        return 'There was a problem updating this workout.'
 
 if __name__ == "__main__":
     app.run(debug=True)
